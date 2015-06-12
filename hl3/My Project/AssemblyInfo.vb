5e66bfa37989e7113d8a36751f51a8ad50ba8f13
keepcalm444 <keepcalm444@gmail.com>
2015-06-12 11:59:04 +1000

    Add initial form

---
+++ hl3/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("hl3")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("DET-NSW")> 
+<Assembly: AssemblyProduct("hl3")> 
+<Assembly: AssemblyCopyright("Copyright © DET-NSW 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("590a7798-a054-48e9-bdcd-185e000c585c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
